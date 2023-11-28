51b95b2ac528b9366d6832dcd55f3f8e474970de
fagpaf <fagpaf@cin.ufpe.br>
2023-11-27 23:19:29 -0300

    função replace

---
+++ Laços de Repetição/8.MTV Video Music Awards 2023.py
@@ -0,0 +1,17 @@
+numero_celebridades = int(input())
+for i in range(1, numero_celebridades + 1):
+  celebridade = input()
+    print(f'Apresentador: Contamos com a ilustre presença de {celebridade}, uma salva de palmas!')
+# Candidatos ao Prêmio VMA
+loop = True
+while loop:
+  nome_candidato = input()
+  if nome_candidato == 'Início da Premiação'
+    loop = False
+possiveis_ganhadores = ['Taylor Swift', 'Katy Perry', 'Ariana Grande', 'Beyoncé', 'Shakira']
+
+
+print(f'Apresentador: Vamos deixar de enrolação e ir para a premiação!')
+print(f'Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...')
+print(f'{.upper}')
+if 
---
+++ Laços de Repetição/9.Os Ex's da Taylor.py
@@ -0,0 +1,11 @@
+loop = True
+while loop:
+nome_pretendente = input()
+palavra_pretendente = input()
+palavra_taylor = input()
+  if palavra_taylor == 'vou dormir':
+    loop = False
+    print(f'você acertou, estreou na lista! {nome_pretendente}')
+    else:
+    print(f'perdeu covarde!')
+    .replace()
