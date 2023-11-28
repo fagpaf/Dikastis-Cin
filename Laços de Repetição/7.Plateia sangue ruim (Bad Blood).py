d52b2fdf9088e37935ea919d5a50173c0fbe941d
fagpaf <fagpaf@cin.ufpe.br>
2023-11-27 22:04:34 -0300

    Update 7.Plateia sangue ruim (Bad Blood).py

--- Laços de Repetição/7.Plateia sangue ruim (Bad Blood).py
+++ Laços de Repetição/7.Plateia sangue ruim (Bad Blood).py
@@ -3,41 +3,27 @@ acerto_plateia = 0
 for i in range(1, num_versos + 1):
   input_plateia = input()
   digito = int(i)
-  if digito == 1:
+  if input_plateia == input_plateia.upper():
+    acerto_plateia += 1
   # 1° verso
+  if digito == 1:
     print("Cause, baby, now we've got")
     print(f'{input_plateia.upper()}')
-    if input_plateia == input_plateia.upper():
-      acerto_plateia += 1
-    else:
-      acerto_plateia += 0
   # 2° verso 
   elif digito == 2:
     print('You know it used to be')
     print(f'{input_plateia.upper()}')
-    if input_plateia == input_plateia.upper():
-      acerto_plateia += 1
-    else:
-      acerto_plateia += 0
   # 3° verso 
   elif digito == 3:
     print('So take a look what')
     print(f'{input_plateia.upper()}')
-    if input_plateia == input_plateia.upper():
-      acerto_plateia += 1
-    else:
-      acerto_plateia += 0
   # 4° verso 
   elif digito == 4:
     print("Cause, baby, now we've got")
     print(f'{input_plateia.upper()}')
-    if input_plateia == input_plateia.upper():
-      acerto_plateia += 1
-    else:
-      acerto_plateia += 0
 if acerto_plateia == num_versos:
     print('A plateia deu um show! Acertou tudo!')
-elif num_versos * 0.5 <= acerto_plateia:
+elif (num_versos * 0.5) <= acerto_plateia:
     print('A plateia acertou a maior parte da música')
 else:
     print('Foi um dia atípico e a plateia se esqueceu de grande da música')
