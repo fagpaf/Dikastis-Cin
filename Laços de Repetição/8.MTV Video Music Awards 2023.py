be38f5ff881c62a84192a3ee03c515a731dc315f
fagpaf <fagpaf@cin.ufpe.br>
2023-11-29 17:09:33 -0300

    Update 8.MTV Video Music Awards 2023.py

--- Laços de Repetição/8.MTV Video Music Awards 2023.py
+++ Laços de Repetição/8.MTV Video Music Awards 2023.py
@@ -8,32 +8,35 @@ while loop:
   nome_candidato = input()
   if nome_candidato == 'Início da Premiação':
     loop = False
-# Premiação
-possivel_ganhadora1 = 'Taylor Swift'
-possivel_ganhadora2 = 'Katy Perry'
-possivel_ganhadora3 = 'Ariana Grande'
-possivel_ganhadora4 = 'Beyoncé'
-possivel_ganhadora5 = 'Shakira'
-print(f'Apresentador: Vamos deixar de enrolação e ir para a premiação!')
-print(f'Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...')
-# Taylor Swift Win
-if nome_candidato == 'Taylor Swift':
-  print(f'{possivel_ganhadora1.upper()}')
-  if celebridade == 'Kanye West':
-    print(f'Kanye West: Eu vou te deixar terminar. Estou feliz por você, mas Beyoncé fez um dos melhores vídeos de todos os tempos.')
-# Katy Parry Win
-elif (nome_candidato == possivel_ganhadora2) and not (nome_candidato == 'Taylor Swift'):
-  print(f'{possivel_ganhadora2.upper()}')
-# Ariana Grande Win
-elif (nome_candidato == possivel_ganhadora3) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2)):
-  print(f'{possivel_ganhadora3.upper()}')
-# Beyoncé Win
-elif (nome_candidato == possivel_ganhadora4) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2) and (nome_candidato == possivel_ganhadora3)):
-  print(f'{possivel_ganhadora4.upper()}')
-  if celebridade == 'Chris Martin':
-    print('Chris Martin: Minha heroína, minha irmã, meu tudo. Você merece!')
-# Shakira Win
-elif (nome_candidato == possivel_ganhadora5) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2) and (nome_candidato == possivel_ganhadora3) and (nome_candidato == possivel_ganhadora4)):
-  print(f'{possivel_ganhadora5.upper()}')
-  if celebridade == 'Gerard Piqué':
-    print(f'Gerard Piqué: Meu amor me perdoa, volta pra mim...')
+  # Premiação
+  repetir= True
+  while repetir:
+    possivel_ganhadora1 = 'Taylor Swift'
+    possivel_ganhadora2 = 'Katy Perry'
+    possivel_ganhadora3 = 'Ariana Grande'
+    possivel_ganhadora4 = 'Beyoncé'
+    possivel_ganhadora5 = 'Shakira'
+    print(f'Apresentador: Vamos deixar de enrolação e ir para a premiação!')
+    print(f'Apresentador: E a artista do ano do MTV Video Music Awards 2023 é...')
+    # Taylor Swift Win
+    if nome_candidato == possivel_ganhadora1:
+      print(f'{possivel_ganhadora1.upper()}')
+      if celebridade == 'Kanye West':
+        print(f'Kanye West: Eu vou te deixar terminar. Estou feliz por você, mas Beyoncé fez um dos melhores vídeos de todos os tempos.')
+        repetir = False
+    # Katy Parry Win
+    elif (nome_candidato == possivel_ganhadora2) and not (nome_candidato == possivel_ganhadora1):
+      print(f'{possivel_ganhadora2.upper()}')
+    # Ariana Grande Win
+    elif (nome_candidato == possivel_ganhadora3) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2)):
+      print(f'{possivel_ganhadora3.upper()}')
+    # Beyoncé Win
+    elif (nome_candidato == possivel_ganhadora4) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2) and (nome_candidato == possivel_ganhadora3)):
+      print(f'{possivel_ganhadora4.upper()}')
+      if celebridade == 'Chris Martin':
+        print('Chris Martin: Minha heroína, minha irmã, meu tudo. Você merece!')
+    # Shakira Win
+    elif (nome_candidato == possivel_ganhadora5) and not ((nome_candidato == possivel_ganhadora1) and (nome_candidato == possivel_ganhadora2) and (nome_candidato == possivel_ganhadora3) and (nome_candidato == possivel_ganhadora4)):
+      print(f'{possivel_ganhadora5.upper()}')
+      if celebridade == 'Gerard Piqué':
+        print(f'Gerard Piqué: Meu amor me perdoa, volta pra mim...')
