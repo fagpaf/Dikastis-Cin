e92503fa056cf2a1d3bdf4d97f372e788abad42a
fagpaf <fagpaf@cin.ufpe.br>
2023-11-30 14:44:40 -0300

    Update 9.Os Ex's da Taylor.py

--- Laços de Repetição/9.Os Ex's da Taylor.py
+++ Laços de Repetição/9.Os Ex's da Taylor.py
@@ -1,11 +1,13 @@
 loop = True
 while loop:
-nome_pretendente = input()
-palavra_pretendente = input()
-palavra_taylor = input()
+  nome_pretendente = input()
+  palavra_pretendente = input()
+  palavra_taylor = input()
   if palavra_taylor == 'vou dormir':
     loop = False
+  for i in(palavra_taylor):
+    palavra_pretendente = palavra_taylor.replace('1', '', 1)
+    print(palavra_pretendente)
     print(f'você acertou, estreou na lista! {nome_pretendente}')
-    else:
+  else:
     print(f'perdeu covarde!')
-    .replace()
