5fc4d79833c78aa27e19eed70f89eebbc5056a58
fagpaf <fagpaf@cin.ufpe.br>
2023-12-05 10:36:17 -0300

    Update 3.A caminho da guerra.py

--- 3.Listas.py/3.A caminho da guerra.py
+++ 3.Listas.py/3.A caminho da guerra.py
@@ -1,9 +1,14 @@
-lista_deseja = ['']
-lista_encontrou = ['']
+#Itens que Procura
+lista_deseja = []
 itens_que_deseja = input()
+lista_deseja = itens_que_deseja.split(', ')
+x = int(len(lista_deseja))
+#Itens Encontrados no Acampamento 
+lista_encontrou = []
 itens_que_encontrou = input()
-lista_deseja += itens_que_deseja.split(', ')
-lista_encontrou += itens_que_encontrou.split(', ')
+lista_encontrou = itens_que_encontrou.split(', ')
+y = int(len(lista_encontrou))
+if x > y:
 print('Estes são os itens que já tenho no Acampamento Meio-Sangue:')
-
-for i, item in enumerate
+  for i in range(1, x+1):
+    print(f"{i}° item: {lista_deseja.idx(i)}")
