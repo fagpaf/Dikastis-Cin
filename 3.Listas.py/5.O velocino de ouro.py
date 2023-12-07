cfd43904d0e8a055dbadd60eea2cfe25efd6e82b
fagpaf <fagpaf@cin.ufpe.br>
2023-12-07 14:47:43 -0300

    q5

--- 3.Listas.py/5.O velocino de ouro.py
+++ 3.Listas.py/5.O velocino de ouro.py
@@ -1,13 +1,4 @@
-informacoes_deuses = [
-    ['Zeus', 'Poseidon', 'Atenas', 'Ares', 'Afrodite'],
-    [100, 90, 80, 70, 60],
-    ['Raio', 'Tridente', 'Égide', 'Lança', 'Cinto Mágico']
-]
-sequencia = int(input()).split(', ')
-lista_algarismos = []
-i = 0
-while i < len():
-  lista_algarismos.append(i)
-  i = i + 1
-if lista_algarismos.index(0):
-  print('Deus: Zeus\nPoder:100\nArtefato:Raio')
+informacoes_deuses = [['Zeus', 'Poseidon', 'Atenas', 'Ares', 'Afrodite'], [100, 90, 80, 70, 60], ['Raio', 'Tridente', 'Égide', 'Lança', 'Cinto Mágico']]
+sequencia = input()
+for n in sequencia:
+  print(f'Deus:{informacoes_deuses[0][int(n)]}')
