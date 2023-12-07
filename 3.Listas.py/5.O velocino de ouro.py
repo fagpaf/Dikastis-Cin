38857ad9642cea4eb08880e07be33b5ec95f3f1a
fagpaf <fagpaf@cin.ufpe.br>
2023-12-06 23:25:09 -0300

    Update 5.O velocino de ouro.py

---
+++ 3.Listas.py/5.O velocino de ouro.py
@@ -0,0 +1,6 @@
+informacoes_deuses = [
+    ['Zeus', 'Poseidon', 'Atenas', 'Ares', 'Afrodite'],
+    [100, 90, 80, 70, 60],
+    ['Raio', 'Tridente', 'Égide', 'Lança', 'Cinto Mágico']
+]
+sequencia = input()
