7b46a45ae938e5ef9787fd74bcd19299d7ed7e0d
fagpaf <fagpaf@cin.ufpe.br>
2023-12-08 09:38:05 -0300

    Update 4.Percy Jackson e a maldição da saga completa.py

--- 3.Listas.py/4.Percy Jackson e a maldição da saga completa.py
+++ 3.Listas.py/4.Percy Jackson e a maldição da saga completa.py
@@ -14,14 +14,14 @@ for l in colecao_sergio:
   if l not in livros_percy:
     livros_outras_sagas.append(l)
 # Outputs
-if qtd_total_edicoes != 0:
+if qtd_total_edicoes > 0:
   if lista_verificacao == livros_percy:
     print('Sua coleção está completa! Você pode ler à vontade.')
-  if len(livros_faltando) > 0:
+  if 5 > len(livros_faltando) > 0:
     print(f'Infelizmente, sua coleção está incompleta. Falta(m) esse(s) livro(s): {", ".join(livros_faltando)}.')
   if len(lista_verificacao) == 0:
     print(f'Caramba, você não tem nenhum livro. Compre todos imediatamente.')
-  if len(livros_outras_sagas) > 0:
+  if len(livros_outras_sagas) != 0:
     print(f'Cuidado, Sérgio! Você está organizando seus livros de uma forma errada, o(s) livro(s): {", ".join(livros_outras_sagas)}, não faz(em) parte da saga "Percy Jackson e os Olimpianos".')
 else:
   print(f'Caramba, você não tem nenhum livro. Compre todos imediatamente.')
