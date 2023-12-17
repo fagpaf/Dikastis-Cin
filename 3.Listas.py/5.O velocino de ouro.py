0c41e7e490ad61799aa52ec69b9acb1868ecba5a
fagpaf <fagpaf@cin.ufpe.br>
2023-12-17 17:45:41 -0300

    Update 5.O velocino de ouro.py

--- 3.Listas.py/5.O velocino de ouro.py
+++ 3.Listas.py/5.O velocino de ouro.py
@@ -5,25 +5,29 @@ informacoes_deuses = [
 ]
 sequencia = input()
 lista_algarismos = []
+idx = 0
 for i in sequencia:
   lista_algarismos.append(int(i))
 # Outputs, iterando o idx do último item da lista
-for idx, n in enumerate(lista_algarismos):
+for n in lista_algarismos:
   if (0 <= n < 2) or (n == 3):
     print(f'Deus:{informacoes_deuses[0][n]}')
     print(f'Poder:{informacoes_deuses[1][n]}')
     print(f'Artefato:{informacoes_deuses[2][n]}')
     if idx != len(lista_algarismos) - 1:
       print()
+      idx += 1
   elif n == 2:
     print(f'Deusa:{informacoes_deuses[0][n]}')
     print(f'Poder:{informacoes_deuses[1][n]}')
     print(f'Artefato:{informacoes_deuses[2][n]}')
     if idx != len(lista_algarismos) - 1:
       print()
+      idx += 1
   elif n == 4:
     print(f'Deusa:{informacoes_deuses[0][n]}')
     print(f'Poder:{informacoes_deuses[1][n]}')
     print(f'Artefato:{informacoes_deuses[2][n]}')
     if idx != len(lista_algarismos) - 1:
       print()
+      idx += 1
