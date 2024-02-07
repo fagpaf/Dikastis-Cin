ab0356ded90a0738d3d38a057f1052f937fc91cc
fagpaf <fagpaf@cin.ufpe.br>
2024-02-07 16:49:39 -0300

    Update teste livro.py

--- LIVRO_IP.py/teste livro.py
+++ LIVRO_IP.py/teste livro.py
@@ -1,25 +1,34 @@
 def conferir_bits(bit):
-    bit = list(bit)
-    if len(bit) == 32:
-        return ''.join(bit)
-    else:
-        bit.insert(0, '0')
-        return conferir_bits(bit)
+  bit = list(bit)
+  if len(bit) == 32:
+    return ''.join(bit)
+  else:
+    bit.insert(0, '0')
+    return conferir_bits(bit)
 
-def verificacao_de_chave(firewall, byte, chances):
-    if chances > 0:
-        if byte in firewall:
-            return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
+def fatiamento(firewall, byte, chances):
+  if chances > 0:
+    
+    def():
+        if byte == firewall[:7]:
+        return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
         else:
-            chances -= 1
-            print("Não é essa a senha, estamos ficando sem tempo.")
-            return verificacao_de_chave(firewall, byte, chances)
-    else:
-        print("Corre Keanu! Eles nos descobriram!!")
-        return "Acesso negado"
+        if len(firewall) > 7:  
+            firewall = firewall[1:]
+            return fatiamento(firewall, byte, chances)
+        else:
 
-palavra = input("Digite a palavra do firewall: ")
-limite_tentativas = int(input("Digite o limite de tentativas: "))
-senha = input("Digite a senha: ")
-acesso = verificacao_de_chave(conferir_bits(palavra), senha, limite_tentativas)
+
+        print("Não é essa a senha, estamos ficando sem tempo.")
+        chances -= 1
+        
+  else:
+    return "Corre Keanu! Eles nos descobriram!!"
+
+palavra = input()
+limite_tentativas = int(input())
+senha = input()
+acesso = fatiamento(conferir_bits(palavra), senha, limite_tentativas)
 print(acesso)
+
+def verificação_de_chave():
