0bfcce7c9ef5ecbf852629e2d240ace7083976ed
fagpaf <fagpaf@cin.ufpe.br>
2024-02-09 11:58:50 -0300

    foda

--- LIVRO_IP.py/sla.py
+++ LIVRO_IP.py/sla.py
@@ -6,13 +6,29 @@ def conferir_bits(bit):
     bit.insert(0, '0')
     return conferir_bits(bit)
 
-def verificação_de_chave(firewall, byte, chances):
-    if byte == firewall[:8]:
-      return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
 
+def fatiamento(firewall, byte, chances):
+  if len(firewall) > 7:  
+    firewall = firewall[1:]
+    return verificacao_de_chave(firewall, byte, chances)
+  else:
+    x = chances - 1 
+    if chances > 0:
+      print("Não é essa a senha, estamos ficando sem tempo.")
+      return x
+    else:
+      return "Corre Keanu! Eles nos descobriram!!"
+
+
+def verificacao_de_chave(firewall, byte, chances):
+  if byte == firewall[:8]:
+    return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
+  else:
+    return fatiamento(firewall, byte, chances)
 
 palavra = input()
 limite_tentativas = int(input())
 senha = input()
-acesso = verificação_de_chave(conferir_bits(palavra), senha, chances)
+parede_de_fogo = conferir_bits(palavra)
+acesso = verificacao_de_chave(parede_de_fogo, senha, limite_tentativas)
 print(acesso)
--- LIVRO_IP.py/teste livro.py
+++ LIVRO_IP.py/teste livro.py
@@ -6,27 +6,34 @@ def conferir_bits(bit):
     bit.insert(0, '0')
     return conferir_bits(bit)
 
-def tentativa(chances):
-  if chances > 0:
-    def verificação_de_chave(firewall, byte, chances):
-      nonlocal chances
-      if byte == firewall[:8]:
-        return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
-      else:
-        if len(firewall) > 7:  
-          firewall = firewall[1:]
-          return verificação_de_chave(firewall, byte, chances)
-        else:
-          chances -= 1
-          return "Não é essa a senha, estamos ficando sem tempo." 
+
+def verificacao_de_chave(firewall, byte, chances, repeticao):
+  global chances
+  if byte == firewall[:8]:
+    return "Muito bem! Estamos dentro! Vamos queimar essa cidade!!"
   else:
-    return "Corre Keanu! Eles nos descobriram!!"
+    if len(firewall) > 7:  
+      firewall = firewall[1:]
+      return verificação_de_chave(firewall, byte, chances)
+    else:
+      repeticao = False
+      chances -= 1
+      return "Não é essa a senha, estamos ficando sem tempo." 
+
 
 palavra = input()
 limite_tentativas = int(input())
 senha = input()
 parede_de_fogo = conferir_bits(palavra)
-funcao_principal = tentativa(limite_tentativas)
-acesso = funcao_principal(parede_de_fogo, senha, limite_tentativas)
-print(acesso)
+acesso = verificação_de_chave(parede_de_fogo, senha, limite_tentativas, loop)
+
+while loop:
+  if limite_tentativas > 0:
+    print(acesso)
+  else:
+    print("Corre Keanu! Eles nos descobriram!!")
+    loop = False
+
+if limite_tentativas != 0:
+  print(acesso)
 
